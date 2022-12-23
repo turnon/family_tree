6bacc801488e9007435ed6efe60fd051cc370493
ken <block24block@gmail.com>
2022-12-23 23:33:03 +0800

    print classes family tree with tree_graph

--- lib/family_tree.rb
+++ lib/family_tree.rb
@@ -1,4 +1,48 @@
 require "family_tree/version"
+require "tree_graph"
 
 module FamilyTree
+  class Node
+    include TreeGraph
+
+    attr_reader :klass, :descendants
+
+    alias_method :label_for_tree_graph, :klass
+    alias_method :children_for_tree_graph, :descendants
+
+    def initialize(klass)
+      @klass = klass
+      @descendants = []
+    end
+  end
+
+  class << self
+    def of(klasses)
+      root = Node.new(::BasicObject)
+
+      Array(klasses).each do |k|
+        parent = root
+
+        k.ancestors.reverse_each do |a|
+          next unless Class === a
+          next if ::BasicObject == a
+
+          child = parent.descendants.detect{ |d| d.klass == a }
+
+          unless child
+            child = Node.new(a)
+            parent.descendants << child
+          end
+
+          parent = child
+        end
+      end
+
+      root
+    end
+
+    def graph(klasses)
+      of(klasses).tree_graph
+    end
+  end
 end
--- test/family_tree_test.rb
+++ test/family_tree_test.rb
@@ -4,4 +4,32 @@ class FamilyTreeTest < Minitest::Test
   def test_that_it_has_a_version_number
     refute_nil ::FamilyTree::VERSION
   end
+
+  class A; end
+
+  class B; end
+
+  class C < B;end
+
+  module M; end
+
+  module N; end
+
+  class D < C
+    include M
+    prepend N
+  end
+
+  def test_family_tree
+    tree = <<~EOS
+      BasicObject
+      └─Object
+        ├─FamilyTreeTest::A
+        └─FamilyTreeTest::B
+          └─FamilyTreeTest::C
+            └─FamilyTreeTest::D
+    EOS
+
+    assert_equal tree.chomp, FamilyTree.graph([A, B, C, D, M])
+  end
 end
